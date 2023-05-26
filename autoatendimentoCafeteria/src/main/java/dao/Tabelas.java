99b3d0ef84863e7fd71b480e7a247ca87f198be8
Matheus Gomes <115513930+MatheusGomes0@users.noreply.github.com>
2023-05-25 22:50:09 -0300

    Add files via upload
    Adicionado a classe e a tela para cadastrar uma nova categoria de um produto

--- autoatendimentoCafeteria/src/main/java/dao/Tabelas.java
+++ autoatendimentoCafeteria/src/main/java/dao/Tabelas.java
@@ -8,18 +8,20 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author mathe
- * Classe para criar as tabelas do banco de dados
+ * @author mathe Classe para criar as tabelas do banco de dados
  */
 public class Tabelas {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         try {
             String clienteTabela = "crete table Cliente(id int AUTO_INCEMENTE primary key,"
                     + "nome varchar(200) not null, email varchar(100) not null, telefone varchar(11) not null, "
                     + "endereco varchar (100) not null, senha varchar(200) not null, confirmarSenha varchar(200) not null, UNIQUE(email) ";
+            String categoriaTabela = "create table categoria (id int AUTO_INCREMENT primary key, nomeCategoria varchar(200))";
             BDOperacoes.setInformacao(clienteTabela, "Tabela Cliente criada com sucesso");
+            BDOperacoes.setInformacao(categoriaTabela, "Tabela Categoria criada com sucesso");
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null,e);
+            JOptionPane.showMessageDialog(null, e);
         }
     }
 }
